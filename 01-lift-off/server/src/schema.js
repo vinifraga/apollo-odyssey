8c4f7690f3b906b0a7dec61151c0aa6e27cb45e7
Vini Fraga <vinifragam@gmail.com>
2022-04-06 17:09:03 -0300

    M01A04: Building our schema

---
+++ 01-lift-off/server/src/schema.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    "Query to get tracks array for the homepage grid"
+    tracksForHome: [Track!]!
+  }
+
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track{
+    id: ID!
+    "the track's title"
+    title: String!
+    "the track's main author"
+    author: Author!
+    "the track's main illustration to display in track card or track page detail"
+    thumbnail: String
+    "the track's approximate length to complete, in minutes"
+    length: Int
+    "the number of modules this track contains"
+    modulesCount: Int
+  }
+
+  "Author of a complete track"
+  type Author{
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture url"
+    photo: String
+  }
+`;
+
+module.exports = typeDefs;
